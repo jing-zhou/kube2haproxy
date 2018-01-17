0e7781b09720c9c05e9267efa6044a87f5678473
jingzhou <jingzhou_wang@hotmail.com>
2018-01-17 21:49:35 +0800

    controller.go:  import from Repository Staging Area

--- proxy/controller/controller.go
+++ proxy/controller/controller.go
@@ -10,13 +10,13 @@ import (
 	"github.com/jing-zhou/kube2haproxy/proxy/template"
 	khcache "github.com/jing-zhou/kube2haproxy/util/cache"
 
-	"k8s.io/kubernetes/pkg/api"
-	"k8s.io/kubernetes/pkg/client/cache"
-	kubeclient "k8s.io/kubernetes/pkg/client/unversioned"
-	"k8s.io/kubernetes/pkg/fields"
-	utilruntime "k8s.io/kubernetes/pkg/util/runtime"
-	"k8s.io/kubernetes/pkg/util/wait"
-	"k8s.io/kubernetes/pkg/watch"
+	"k8s.io/apimachinery/pkg/fields"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/apimachinery/pkg/watch"
+	kubeclient "k8s.io/client-go/kubernetes"
+	api "k8s.io/client-go/pkg/api/v1"
+	"k8s.io/client-go/tools/cache"
 
 	"github.com/golang/glog"
 )
